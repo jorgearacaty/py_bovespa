27cce470a1701d1169bb19c538fe4b968d9dee2e
jorgearacaty <jorgearacaty@gmail.com>
2014-08-08 12:16:41 -0300

    CCE 20140808

--- py_COTAHIST_to_CSV.py
+++ py_COTAHIST_to_CSV.py
@@ -1,31 +1,29 @@
 # -*- coding: cp1252 -*-
-# -------------------------------------------------------
-# --- Python.
+# ---------------------------------------------------------------------
+# --- Python 2.7.3.
 # --- py_COTAHIST_to_CSV.py - Copia arq de cotação histótica da Bovespa
 # --- em formato campo fixo, para o formato CSV.
 # --- criado em - jorgearacaty, 4 jan 2014 - 2312.
-# --- jorgearacaty, 7 ago 2014 - 1612.
-# -------------------------------------------------------
+# --- jorgearacaty, 8 ago 2014 - 1213.
+# ---------------------------------------------------------------------
+
 import os
 import sys
 import csv
-#import mp        # --- importa mp.py na mesma pasta.
 
-# --- I am r_asc_w_csv...
-# --- Lê arquivo ASCII campos de tamanho fixo sem delimitadores,
-# --- com cotações histórica, arquivo baixado do site bovespa e
-# --- transcreve para arquivo .csv.
-# ------------------
+# ----------------------------------------------------------
+# Lê arquivo ASCII campos de tamanho fixo sem delimitadores,
+# com cotações histórica, arquivo baixado do site bovespa e
+# transcreve para arquivo .csv.
+# ----------------------------------------------------------
 def r_asc_w_csv(nome_arq):
     '''
     Beazley pags, 13, 158. 
     http://www.pythonforbeginners.com/systems-programming/using-the-csv-module-in-python/
     '''
-    #print '\nI am r_asc_w_csv...\n'
     file_name = nome_arq + '.txt'
 
-    # --- matriz que define a estrutura do registro.
-    # ---
+    # matriz que define a estrutura do registro.
     ahd = [(1,2),(3,10),(11,12),
            (13,24),(25,27),(28,39),
            (40,49),(50,52),(53,56),
@@ -42,40 +40,33 @@ def r_asc_w_csv(nome_arq):
     print '\nEscrevendo arquivo ->', nome_arq + '.csv'
     npros = 0
         
-    # --- lê e escreve registros csv.
-    # -------------------------------
+    # read tamanho fix, write csv.
     for line in open(file_name):
-        
         registro = ''
-        
         for lim in xrange(len(ahd)):    # --- itera nº de campos do reg. vezes.
-            
             if lim == 0:
-                # --- primeiro campo não tem vírgula na frente.
+                # primeiro campo não tem vírgula na frente.
                 registro = registro + line[(-1)+ahd[lim][0]:ahd[lim][1]]
             else:
                 registro = registro + ',' + line[(-1)+ahd[lim][0]:ahd[lim][1]]
-                
         registro = registro + '\n'
         ot.write(registro)
-        # print registro # --- opcional
         npros = npros+1
         
     print '\nprocessados ->', npros, '<- registros'
 
-# --- início padrão.
-# ------------------
+# -----------------------------------------------
+# Uma grande jornada inicia com o primeiro passo.
+# -----------------------------------------------
 if __name__ == '__main__':
 
     print 'Python running... '
     print sys.argv[0]
-
-    #mp.pt_me() # --- chama função em outro arquivo (mp.py).
     
     r_asc_w_csv('COTAHIST_A2013')
     
     print "\nDONE"
-    # The_End = raw_input("press any key to go out, thks.\n")
+
 
 
 
